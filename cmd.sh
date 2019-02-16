2cdd9c5f8eb09933dd99a55ccf1e3c37df54ddd9
zachpuck <zachpuckett@gmail.com>
2019-02-15 16:58:54 -0800

    init

---
+++ cmd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+echo "checking if kind cluster already exists"
+
+CLUSTER_LIST=$(kind get clusters)
+
+for c in ${CLUSTER_LIST}; do
+    if [ "${c}" = "${name}" ]; then
+        clusterExists=true
+    fi
+done
+
+if [ ${clusterExists} ]; then
+    echo "kind cluster already exists, use the existing kubeconfig: kind-config-$name"
+    echo "if you no longer have access to the kind clusters kubeconfig please create a new cluster"
+else
+    echo "creating kind cluster"
+
+    kind create cluster --config /clusterConfig.yaml --name ${name}
+    cat $(kind get kubeconfig-path --name="${name}") > /outputDir/kind-config-${name}.yaml
+fi
